3c3678b52988bafa944e31cf84ce166857b4438c
Jason-dahang005 <jasondahang005@gmail.com>
2022-09-09 04:15:34 +0800

    navbar

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/client/header/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='container mx-auto'>
+      <Navbar/>
     </div>
   );
 }
