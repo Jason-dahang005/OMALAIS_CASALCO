06014268dd831e827d8115c5d0fe16070530c842
Jason-dahang005 <jasondahang005@gmail.com>
2022-09-12 19:47:48 +0800

    go go gohgjhgh

--- capstone_project/resources/views/client/home.blade.php
+++ capstone_project/resources/views/client/home.blade.php
@@ -7,63 +7,63 @@
   <div class="container-fluid">
       <div class="row">
           <div class="col-lg-4">
-              <div class="left-content">
-                  <div class="inner-content">
-                      <h4>CASALCO</h4>
-                      <h6>THE BEST EXPERIENCE</h6>
-                      <div class="main-white-button scroll-to-section">
-                          <a href="#reservation">Become A Member</a>
-                      </div>
-                  </div>
-              </div>
+						<div class="left-content">
+							<div class="inner-content">
+								<h4>CASALCO</h4>
+								<h6>THE BEST EXPERIENCE</h6>
+								<div class="main-white-button scroll-to-section">
+									<a href="/membership-application">Become A Member</a>
+								</div>
+							</div>
+						</div>
           </div>
           <div class="col-lg-8">
-              <div class="main-banner header-text">
-                  <div class="Modern-Slider">
-                    <!-- Item -->
-                    <div class="item">
-                      <div class="img-fill">
-                          <img src="../klassy/images/Casalco-1.jpg" alt="">
-                      </div>
-                    </div>
-                    <!-- // Item -->
-                    <!-- Item -->
-                    <div class="item">
-                      <div class="img-fill">
-                          <img src="../klassy/images/Casalco-4.jpg" alt="">
-                      </div>
-                    </div>
-                    <!-- // Item -->
-                    <!-- Item -->
-                    <div class="item">
-                      <div class="img-fill">
-                          <img src="../klassy/images/Casalco-3.jpg" alt="">
-                      </div>
-                    </div>
-                    <!-- // Item -->
-                    <!-- Item -->
-                    <div class="item">
-                      <div class="img-fill">
-                          <img src="../klassy/images/Casalco-5.jpg" alt="">
-                      </div>
-                    </div>
-                    <!-- // Item -->
-                    <!-- Item -->
-                    <div class="item">
-                      <div class="img-fill">
-                          <img src="../klassy/images/Casalco-6.jpg" alt="">
-                      </div>
-                    </div>
-                    <!-- // Item -->
-                    <!-- Item -->
-                    <div class="item">
-                      <div class="img-fill">
-                          <img src="../klassy/images/Casalco-7.jpg" alt="">
-                      </div>
-                    </div>
-                    <!-- // Item -->
-                  </div>
-              </div>
+						<div class="main-banner header-text">
+							<div class="Modern-Slider">
+								<!-- Item -->
+								<div class="item">
+									<div class="img-fill">
+										<img src="../klassy/images/Casalco-1.jpg" alt="">
+									</div>
+								</div>
+								<!-- // Item -->
+								<!-- Item -->
+								<div class="item">
+									<div class="img-fill">
+										<img src="../klassy/images/Casalco-4.jpg" alt="">
+									</div>
+								</div>
+								<!-- // Item -->
+								<!-- Item -->
+								<div class="item">
+									<div class="img-fill">
+										<img src="../klassy/images/Casalco-3.jpg" alt="">
+									</div>
+								</div>
+								<!-- // Item -->
+								<!-- Item -->
+								<div class="item">
+									<div class="img-fill">
+										<img src="../klassy/images/Casalco-5.jpg" alt="">
+									</div>
+								</div>
+								<!-- // Item -->
+								<!-- Item -->
+								<div class="item">
+									<div class="img-fill">
+										<img src="../klassy/images/Casalco-6.jpg" alt="">
+									</div>
+								</div>
+								<!-- // Item -->
+								<!-- Item -->
+								<div class="item">
+									<div class="img-fill">
+										<img src="../klassy/images/Casalco-7.jpg" alt="">
+									</div>
+								</div>
+								<!-- // Item -->
+							</div>
+						</div>
           </div>
       </div>
   </div>
@@ -73,92 +73,31 @@
 <!-- ***** Menu Area Starts ***** -->
 <section class="section" id="menu">
   <div class="container">
-      <div class="row">
-          <div class="col-lg-4">
-              <div class="section-heading">
-                  <h6>Our Menu</h6>
-                  <h2>Our selection of cakes with quality taste</h2>
-              </div>
-          </div>
-      </div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="section-heading">
+					<h6>Our Menu</h6>
+					<h2>Our selection of cakes with quality taste</h2>
+				</div>
+			</div>
+		</div>
   </div>
   <div class="menu-item-carousel">
-      <div class="col-lg-12">
-          <div class="owl-menu-item owl-carousel">
-               <div class="item">
-                  <div class='card card1'>
-                      <div class="price"><h6>$14</h6></div>
-                      <div class='info'>
-                        <h1 class='title'>Chocolate Cake</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                        <div class="main-text-button">
-                            <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-             {{-- <div class="item">
-                  <div class='card card2'>
-                      <div class="price"><h6>$22</h6></div>
-                      <div class='info'>
-                        <h1 class='title'>Klassy Pancake</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                        <div class="main-text-button">
-                            <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="item">
-                  <div class='card card3'>
-                      <div class="price"><h6>$18</h6></div>
-                      <div class='info'>
-                        <h1 class='title'>Tall Klassy Bread</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                        <div class="main-text-button">
-                            <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="item">
-                  <div class='card card4'>
-                      <div class="price"><h6>$10</h6></div>
-                      <div class='info'>
-                        <h1 class='title'>Blueberry CheeseCake</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                        <div class="main-text-button">
-                            <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="item">
-                  <div class='card card5'>
-                      <div class="price"><h6>$8.50</h6></div>
-                      <div class='info'>
-                        <h1 class='title'>Klassy Cup Cake</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                        <div class="main-text-button">
-                            <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="item">
-                  <div class='card card3'>
-                      <div class="price"><h6>$7.25</h6></div>
-                      <div class='info'>
-                        <h1 class='title'>Klassic Cake</h1>
-                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                        <div class="main-text-button">
-                            <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                        </div>
-                      </div>
-                  </div>
-              </div> --}}
-          </div>
-      </div>
+		<div class="col-lg-12">
+			<div class="owl-menu-item owl-carousel">
+				<div class="item">
+				<div class='card card1'>
+					<div class="price"><h6>$14</h6></div>
+					<div class='info'>
+						<h1 class='title'>Chocolate Cake</h1>
+						<p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
+						<div class="main-text-button">
+							<div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
   </div>
 </section>
 <!-- ***** Menu Area Ends ***** -->
@@ -166,67 +105,67 @@
 <!-- ***** Chefs Area Starts ***** -->
 <section class="section" id="chefs">
   <div class="container">
-      <div class="row">
-          <div class="col-lg-4 offset-lg-4 text-center">
-              <div class="section-heading">
-                  <h6>Our Chefs</h6>
-                  <h2>We offer the best ingredients for you</h2>
-              </div>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-lg-4">
-              <div class="chef-item">
-                  <div class="thumb">
-                      <div class="overlay"></div>
-                      <ul class="social-icons">
-                          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                      </ul>
-                      <img src="../klassy/images/chefs-01.jpg" alt="Chef #1">
-                  </div>
-                  <div class="down-content">
-                      <h4>Randy Walker</h4>
-                      <span>Pastry Chef</span>
-                  </div>
-              </div>
-          </div>
-          <div class="col-lg-4">
-              <div class="chef-item">
-                  <div class="thumb">
-                      <div class="overlay"></div>
-                      <ul class="social-icons">
-                          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                          <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                      </ul>
-                      <img src="../klassy/images/chefs-02.jpg" alt="Chef #2">
-                  </div>
-                  <div class="down-content">
-                      <h4>David Martin</h4>
-                      <span>Cookie Chef</span>
-                  </div>
-              </div>
-          </div>
-          <div class="col-lg-4">
-              <div class="chef-item">
-                  <div class="thumb">
-                      <div class="overlay"></div>
-                      <ul class="social-icons">
-                          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                          <li><a href="#"><i class="fa fa-google"></i></a></li>
-                      </ul>
-                      <img src="../klassy/images/chefs-03.jpg" alt="Chef #3">
-                  </div>
-                  <div class="down-content">
-                      <h4>Peter Perkson</h4>
-                      <span>Pancake Chef</span>
-                  </div>
-              </div>
-          </div>
-      </div>
+		<div class="row">
+			<div class="col-lg-4 offset-lg-4 text-center">
+				<div class="section-heading">
+					<h6>Our Chefs</h6>
+					<h2>We offer the best ingredients for you</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="chef-item">
+					<div class="thumb">
+						<div class="overlay"></div>
+						<ul class="social-icons">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+						<img src="../klassy/images/chefs-01.jpg" alt="Chef #1">
+					</div>
+					<div class="down-content">
+						<h4>Randy Walker</h4>
+						<span>Pastry Chef</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="chef-item">
+					<div class="thumb">
+						<div class="overlay"></div>
+						<ul class="social-icons">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+						</ul>
+						<img src="../klassy/images/chefs-02.jpg" alt="Chef #2">
+					</div>
+					<div class="down-content">
+						<h4>David Martin</h4>
+						<span>Cookie Chef</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="chef-item">
+					<div class="thumb">
+						<div class="overlay"></div>
+						<ul class="social-icons">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google"></i></a></li>
+						</ul>
+						<img src="../klassy/images/chefs-03.jpg" alt="Chef #3">
+					</div>
+					<div class="down-content">
+						<h4>Peter Perkson</h4>
+						<span>Pancake Chef</span>
+					</div>
+				</div>
+			</div>
+		</div>
   </div>
 </section>
 <!-- ***** Chefs Area Ends ***** -->
